3bd0ae5dd36e7e928a41e0c63eadecb6b2d471d5
Eduard Tungatarov <eduard.tungatarov@yandex.ru>
2025-12-03 22:55:07 +0500

    подключили логер

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,14 +3,23 @@ package main
 import (
 	"context"
 	"database/sql"
-	"log"
 	"os/signal"
 	"syscall"
+
+	"github.com/eduardtungatarov/goph-keeper/internal/logger"
+
+	_ "github.com/jackc/pgx/v5/stdlib"
 )
 
 func main() {
 	_, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
+
+	// Логер.
+	log, err := logger.MakeLogger()
+	if err != nil {
+		panic(err)
+	}
 
 	// Подключаемся к базе данных.
 	db, err := sql.Open("pgx", "DSN")
