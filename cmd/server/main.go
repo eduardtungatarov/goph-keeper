3b31717d9f3c4d4e9b6f42b2b75b3d47e99eb40c
Eduard Tungatarov <eduard.tungatarov@yandex.ru>
2025-12-03 22:43:45 +0500

    подключение к бд

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"database/sql"
+	"log"
 	"os/signal"
 	"syscall"
 )
@@ -9,4 +11,11 @@ import (
 func main() {
 	_, stop := signal.NotifyContext(context.Background(), syscall.SIGINT, syscall.SIGTERM)
 	defer stop()
+
+	// Подключаемся к базе данных.
+	db, err := sql.Open("pgx", "DSN")
+	if err != nil {
+		log.Fatalf("Failed to open DB: %v", err)
+	}
+	defer db.Close()
 }
